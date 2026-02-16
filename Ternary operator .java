b78738638bbf3289b924957c48df6bfba366d69b
ThirumaranV337 <thirumarant337@gmail.com>
2026-02-16 16:39:26 +0530

    Add ternary operator example in Main class
    Implement a ternary operator to determine result based on even or odd value of n.

---
+++ Ternary operator .java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String a[])
+    {
+        int n;
+        int result;
+        n=20;
+        result= n%2==0 ? 10 : 20;
+        System.out.println(result);
+    }
+}
+//variable = (condition) ? expressionTrue : expressionFalse;
