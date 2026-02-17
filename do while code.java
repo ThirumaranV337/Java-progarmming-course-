1c034800eb12e9e6510aceb3e7510648adde3205
ThirumaranV337 <thirumarant337@gmail.com>
2026-02-17 16:50:57 +0530

    Add do-while loop example in Main class
    Implement a do-while loop that prints a message twice.

---
+++ do while code.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String a[])
+    {
+        int i;
+        i=0;
+       do{
+            System.out.println("Hey Thirumaran please stop this  " + i);
+            i++;
+        }while(i<=1);
+    }
+}
+// run atleast once when the condition is false
