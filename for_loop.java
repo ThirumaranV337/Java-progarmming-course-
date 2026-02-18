a5217b9c4e312ada9557cb16dfebadcab254ffe9
ThirumaranV337 <thirumarant337@gmail.com>
2026-02-18 16:48:38 +0530

    Create for_loop.java

---
+++ for_loop.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String a[]){
+        int i;
+        for (i=1;i<=5;i++)
+        {
+            System.out.println("The for loop is running " + i);
+        }
+        
+    }
+}
+/*for (initialization; condition; update) {
+     code to be executed repeatedly
+}*/
