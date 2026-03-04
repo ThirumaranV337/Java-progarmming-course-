10336565a4923230398b2965601cdc7f51e74b53
ThirumaranV337 <thirumarant337@gmail.com>
2026-03-04 16:58:57 +0530

    Create Example progrmafor creation of multidimensional array.java

---
+++ Example progrmafor creation of multidimensional array.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        int arr1[][]=new int[3][4];
+        for (int i=0;i<3;i++)
+        {
+          for (int j=0;j<4;j++){
+              System.out.print(arr1[i][j]+" ");
+              
+          }
+          System.out.println(" ");
+        }
+            
+    }
+}
